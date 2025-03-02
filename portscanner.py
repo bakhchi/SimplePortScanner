06fe869d7d76250723dd2ef1248d9ffcb1b8e7f6
Bakhchi <zachary.bakhchi@cooper.edu>
2025-03-02 00:53:15 -0500

    final touches

--- portscanner.py
+++ portscanner.py
@@ -9,31 +9,6 @@ parser.add_argument("--input", type=str, required=False, help="Read a list of po
 parser.add_argument("--output", type=str, required=False, help="Write the results to a file")
 args = parser.parse_args()
 
-#Checks for arguments and sets variables to default values if not provided
-if args.ports: 
-    ports = args.ports
-else: 
-    ports = range(1, 1025)
-
-if args.ip:
-    serverName = args.ip
-else:
-    serverName = 'localhost'
-
-if args.input:
-    print("Reading from input file:", args.input)
-    eof = False
-    with open(args.input, "r") as f:
-        for line in f:
-            arr = line.split()
-            serverName = arr[0]
-            ports = range(int(arr[1]), int(arr[2]) + 1)
-
-if args.output:
-    print("Logging to output file:", args.output)
-    output = True
-else:
-    output = False
 
 #Known Port Services
 port_services = {
@@ -59,10 +34,9 @@ port_services = {
     25565: "Minecraft"
 }
 
-results = {} #Store results of scan for later
 lock = threading.Lock() #Needed for threading to access resources properly
 
-def scan(port, serverIP): #Scans a port on a server, stores results in dictionary 
+def scan(port, serverIP, results): #Scans a port on a server, stores results in dictionary 
     try:
         clientSocket = socket(AF_INET, SOCK_STREAM)
         clientSocket.settimeout(0.3)
@@ -92,19 +66,70 @@ def scan(port, serverIP): #Scans a port on a server, stores results in dictionar
 
 
 def main(): #Main function
-    serverIP = gethostbyname(serverName) #Get IP of server in case domain name is given 
-    print(f'Scanning {serverIP}')
-    threads = []
-    for port in ports:
-        t = threading.Thread(target=scan, args=(port, serverName)) #Preps threading with scan function
-        threads.append(t)
-        t.start()
-    for thread in threads:
-        thread.join() #Waits for all threads to finish before printing results
-    for port in sorted(results): #If output is enabled, write to file. Otherwise, print to console
-        if(output):
+    #Checks for arguments and sets variables to default values if not provided
+    if args.ports: 
+        ports = args.ports
+    else: 
+        ports = range(1, 1025)
+
+    if args.ip:
+        serverName = args.ip
+    else:
+        serverName = 'localhost'
+
+    if args.input:
+        print("Reading from input file:", args.input)
+        input = True
+    else: 
+        input = False
+    if args.output:
+        print("Logging to output file:", args.output)
+        output = True
+    else:
+        output = False
+
+    if(input): #If input is enabled, read from file
+        with open(args.input, "r") as f:
+            for line in f:
+                results = {} #Store results of scan for later
+                arr = line.split() #Gathers info from line by turning it into an array, disregarding whitespace 
+                serverName = arr[0] 
+                serverIP = gethostbyname(serverName) #Get IP of server in case domain name is given 
+                ports = range(int(arr[1]), int(arr[2]) + 1)
+                if(output): 
+                        with open(args.output, "a") as f:
+                            f.write(f'Scanning {serverName} \n')                
+                print(f'Scanning {serverName}')
+                threads = []
+                for port in ports:
+                    t = threading.Thread(target=scan, args=(port, serverIP, results)) #Preps threading with scan function
+                    threads.append(t)
+                    t.start()
+                for thread in threads:
+                    thread.join() #Waits for all threads to finish before printing results
+                for port in sorted(results): 
+                    if(output): #If output is enabled, write to file. Otherwise, print to console
+                        with open(args.output, "a") as f:
+                            f.write(results[port] + "\n")
+                    else:
+                        print(results[port])       
+    else: #The rest is the same as above, just as if it was a single line in an input file
+        serverIP = gethostbyname(serverName) 
+        print(f'Scanning {serverIP}')
+        if(output): 
             with open(args.output, "a") as f:
-                f.write(results[port] + "\n")
-        else:
-            print(results[port])
+                f.write(f'Scanning {serverName} \n')      
+        threads = []
+        for port in ports:
+            t = threading.Thread(target=scan, args=(port, serverIP)) 
+            threads.append(t)
+            t.start()
+        for thread in threads:
+            thread.join() 
+        for port in sorted(results): 
+            if(output):
+                with open(args.output, "a") as f:
+                    f.write(results[port] + "\n")
+            else:
+                print(results[port])
 main()
